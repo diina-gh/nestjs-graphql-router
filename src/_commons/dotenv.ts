aed50321ba9fad064034cf0ed6e29b2feeb0180d
Seydina <dina3903@gmail.com>
2022-10-10 12:03:04 +0000

    update

---
+++ src/_commons/dotenv.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+
+export const APP_NAME = process.env.APP_NAME 
+export const APP_PORT = process.env.APP_PORT 
+export const MODE = process.env.MODE 
+
+export const AUTH_SUBGRAPH = process.env.AUTH_SUBGRAPH
+export const PRODUCT_SUBGRAPH = process.env.PRODUCT_SUBGRAPH 
+
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        // ... Apollo server options
+        cors: true,
+        debug: true,
+        playground: false,
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'auth', url: 'http://localhost:4001/graphql' },
+            { name: 'product', url: 'http://localhost:4002/graphql' },
+          ],
+        }),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
